4365c3659716e78097ce47009f62f579b73a40d4
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-11 22:08:26 -0500

    completed alphabetize

--- CS 5001/Project2/alphabetize.py
+++ CS 5001/Project2/alphabetize.py
@@ -2,3 +2,16 @@
 # Fall 2023, CS 5001
 # Lab 2 code file
 # Practicing good code style and more complex functions
+
+#ask the user to input four words
+firstword = input("Please provide your first word ")
+secondword = input("Please provide your second word --> ")
+thirdword = input("Please provide your third word --> ")
+fourthword = input("Please provide your fourth word --> ")
+
+#Return a sorted list alphabetically
+Allwords = (firstword, secondword, thirdword, fourthword)
+Sortedwords = sorted(Allwords)
+
+#print the results from what was determined
+print(Sortedwords)
