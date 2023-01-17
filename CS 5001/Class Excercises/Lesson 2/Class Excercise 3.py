cf62de3d655dd0ebf448123ef305349cf5200116
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-17 12:13:32 -0500

    adding class exercise 3

---
+++ CS 5001/Class Excercises/Lesson 2/Class Excercise 3.py
@@ -0,0 +1,21 @@
+# Michael Arthur Mills
+# Fall 2023, CS 5001
+# Class code Problem
+# Practicing good code style and more complex functions
+
+#Determine whether or not a triangle is a right triangle
+def main():
+    
+    x = 5
+    y = 6
+    z = 7
+  
+    if(x**2 + y**2 == z**2):
+        print("This is a right triangle")
+    else:
+        print("This is not a right triangle")
+
+main()
+    
+
+
