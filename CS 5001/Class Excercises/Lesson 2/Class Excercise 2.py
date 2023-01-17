a1b3a7d7e230f89d2210ccd14e608a3f38d60f9a
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-17 11:08:24 -0500

    adding lesson folders and class Excercise

---
+++ CS 5001/Class Excercises/Lesson 2/Class Excercise 2.py
@@ -0,0 +1,26 @@
+# Michael Arthur Mills
+# Fall 2023, CS 5001
+# Class code Problem
+# Practicing good code style and more complex functions
+#Challenge the hello world program so that each of the strings are variables 
+def main():
+    hello = str("Hello ")
+    world = str("World ")
+        
+    print(hello+world)
+
+
+#Now make a new variable that holds your name
+
+    myname = str("Michael")
+    
+#Use these variables to make the program print “Hello World” and “Hello Myname”
+    print(hello+world+myname)
+
+#Write a new program that imitates a 3 year old. Have it print out “why? A number of times using the * operated for stirngs 
+    num_why = int(input("How many why's? --> "))
+    print("why?" * num_why)
+    
+main()
+
+
