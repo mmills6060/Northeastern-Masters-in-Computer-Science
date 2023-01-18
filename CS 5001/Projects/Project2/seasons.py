30e48d31fe524fd0125751942999e24daea805cd
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-18 11:16:24 -0500

    three numbers completed

--- CS 5001/Projects/Project2/seasons.py
+++ CS 5001/Projects/Project2/seasons.py
@@ -2,3 +2,10 @@
 # Fall 2023, CS 5001
 # Lab 2 code file
 # Practicing good code style and more complex functions
+
+
+# Ask the user to provide the date in number form
+
+# Use split function to single out the month
+
+# Single out what month is what season
