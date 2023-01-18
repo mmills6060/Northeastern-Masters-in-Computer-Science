62fbe5dea2ef57fe234e5cd02652e6bbaa9e04ad
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-18 12:32:07 -0500

    completed seasons

--- CS 5001/Projects/Project2/seasons.py
+++ CS 5001/Projects/Project2/seasons.py
@@ -5,8 +5,24 @@
 
 
 # Ask the user to provide the date in number form
-
+def main():
+    date = str(input("Please provide the date in the following format, MM/DD/YYYY --> "))
+    
 # Use split function to single out the month
-
-# Single out what month is what season
-
+    datesplit = date.split("/")
+    month = datesplit[0]
+    
+# Single out what month is what season, print season
+# I am keeping as a string because there is additional complexities 
+# changing to an integer, specifically when there is a zero digit in front
+    if  month in ["12" , "01" , "02"]:
+        print("The season is Winter")
+    else:
+        if  month in ["03" , "04" , "05"]:
+            print("The season is Spring")
+        else:
+            if  month in ["06" , "07" , "08"]:
+                print("The season is Summer")
+            else:
+                print("The season is Fall")        
+main()
