0f74e4fe1aa5e6850af3bf0ddc9ee1c6e52ffbec
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-18 11:17:05 -0500

    three numbers completed

--- CS 5001/Projects/Project2/threenumbers.py
+++ CS 5001/Projects/Project2/threenumbers.py
@@ -4,22 +4,37 @@
 # Practicing good code style and more complex functions
 
 
-
+def main():
 #ask the user to input the numbers
-firstnumber = float(input("Please provide your first number "))
-secondnumber = float(input("Please provide your second number --> "))
-thirdnumber = float(input("Please provide your third number --> "))
+    firstnumber = int(input("Please provide your first number "))
+    secondnumber = int(input("Please provide your second number --> "))
+    thirdnumber = int(input("Please provide your third number --> "))
 
-#Determine in min, max, and average based on the inputted numbers
-max = max(firstnumber, secondnumber, thirdnumber)
-min = min(firstnumber, secondnumber, thirdnumber)
-average = (firstnumber + secondnumber + thirdnumber) / 3
+#Determine the min, max, based on the inputted numbers, print the result
+    if firstnumber>secondnumber:
+        if secondnumber>thirdnumber:
+            print("The biggest number is:", firstnumber)
+            print("The smallest number is:", thirdnumber)
+        else:
+            if firstnumber>thirdnumber:
+                print("The biggest number is:", firstnumber)
+                print("The smallest number is:", secondnumber)
+            else:
+                print("The biggest number is:", thirdnumber)
+                print("The smallest number is:", secondnumber)
+    else: 
+            if secondnumber>thirdnumber:
+                print("The biggest number is:", secondnumber)
+                print("The smallest number is:", thirdnumber)
+            else:
+                if firstnumber>thirdnumber:
+                    print("The biggest number is:", firstnumber)
+                    print("The smallest number is:", thirdnumber)
+                else:
+                    print("The biggest number is:", thirdnumber)
+                    print("The smallest number is:", firstnumber)
 
-#print the results from what was determined
-print("The biggest number is:", max)
-print("The smallest number is:", min)
-print("The average is:", average)
+#print the average
+    print("The average is:", (firstnumber + secondnumber + thirdnumber)/3),
 
-
-# I recommend that you write out your algorithm in comments.
-# Then write the code for each comment directly below it.
+main()
