6bb3678694199b3164a038dde4ed8e4f9f905953
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-19 08:52:33 -0500

    half way completed alphabetize

--- CS 5001/Projects/Project2/alphabetize.py
+++ CS 5001/Projects/Project2/alphabetize.py
@@ -9,9 +9,41 @@ secondword = input("Please provide your second word --> ")
 thirdword = input("Please provide your third word --> ")
 fourthword = input("Please provide your fourth word --> ")
 
-#Return a sorted list alphabetically
-Allwords = (firstword, secondword, thirdword, fourthword)
-Sortedwords = sorted(Allwords)
-
+#determine alphabetical order
+if secondword>thirdword:
+    if secondword>fourthword:           
+        if thirdword>fourthword:
+            if secondword>firstword:
+                if thirdword>firstword:
+                    print(firstword,fourthword,thirdword,secondword)
+                else:
+                    print(fourthword,firstword,thirdword,secondword)
+            else:
+                print(thirdword,fourthword,secondword,firstword)
+        else:
+            if secondword>firstword:
+                if fourthword>firstword:
+                    if thirdword>firstword:
+                        print(firstword,thirdword,fourthword,secondword)
+    else:
+        if fourthword>firstword:
+            if secondword>firstword:
+                if thirdword>firstword:
+                    print(firstword,thirdword,fourthword,secondword)
+        else: 
+            if secondword>firstword:
+                if fourthword>firstword:
+                    if thirdword>firstword:
+                        print(firstword,thirdword,fourthword,secondword)
+            else:
+else:
+    if thirdword>fourthword:
+        if secondword>fourthword:
+            if thirdword>firstword:
+                if secondword>firstword:
+                    print(firstword,secondword,fourthword,thirdword)
+    else:
+        if fourthword>firstword:
+            print(secondword,thirdword,fourthword,firstword)
 #print the results from what was determined
-print(Sortedwords)
+print()
