b671182410a9df914a0d3b87d61c024323cc5d36
Michael Mills <mmills6060@gmail.com>
2023-01-19 11:29:39 -0500

    corrected the in operator that I am not supposed to be using

--- CS 5001/Projects/Project2/seasons.py
+++ CS 5001/Projects/Project2/seasons.py
@@ -1,28 +1,30 @@
 # Michael Arthur Mills
 # Fall 2023, CS 5001
 # Lab 2 code file
-# Practicing good code style and more complex functions
+# Ask the user for a date, in number form. 
+#  Then use this information to print out the season that date 
+# happens in (ie 1/18/2022 is in Winter).
 
 
 # Ask the user to provide the date in number form
 def main():
     date = str(input("Please provide the date in the following format, MM/DD/YYYY --> "))
     
-# Use split function to single out the month
+# Use split function to separate into three different values
+#based on where the "/" is inputted.
     datesplit = date.split("/")
+
+#Defining the first value in the set as month
     month = datesplit[0]
     
 # Single out what month is what season, print season
-
-# I am keeping as a string because there are additional complexities 
-# changing to an integer, specifically when there is a zero digit in front
-    if  month in ["12" , "01" , "02"]:
+    if  month == '12' or month == '01' or month == '02':
         print("The season is Winter")
     else:
-        if  month in ["03" , "04" , "05"]:
+        if  month == '03' or month == '04' or month == '05':
             print("The season is Spring")
         else:
-            if  month in ["06" , "07" , "08"]:
+            if  month == '06' or month == '07' or month == '08':
                 print("The season is Summer")
             else:
                 print("The season is Fall")        
