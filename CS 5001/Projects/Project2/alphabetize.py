cf36a5cb8e1d129182175c5c07fe9643458058ec
Michael Mills <mmills6060@gmail.com>
2023-01-19 09:27:43 -0500

    alphabetize completed

--- CS 5001/Projects/Project2/alphabetize.py
+++ CS 5001/Projects/Project2/alphabetize.py
@@ -9,7 +9,7 @@ secondword = input("Please provide your second word --> ")
 thirdword = input("Please provide your third word --> ")
 fourthword = input("Please provide your fourth word --> ")
 
-#determine alphabetical order
+#determine alphabetical order and print
 if secondword>thirdword:
     if secondword>fourthword:           
         if thirdword>fourthword:
@@ -25,25 +25,59 @@ if secondword>thirdword:
                 if fourthword>firstword:
                     if thirdword>firstword:
                         print(firstword,thirdword,fourthword,secondword)
+                    else:
+                        print(thirdword,firstword,fourthword,secondword)
+                else:
+                    print(thirdword,fourthword,firstword,secondword)
+            else:
+                print(thirdword,fourthword,secondword,firstword)
     else:
         if fourthword>firstword:
             if secondword>firstword:
                 if thirdword>firstword:
                     print(firstword,thirdword,fourthword,secondword)
-        else: 
-            if secondword>firstword:
-                if fourthword>firstword:
-                    if thirdword>firstword:
-                        print(firstword,thirdword,fourthword,secondword)
+                else:
+                    print(thirdword,firstword,fourthword,secondword)
             else:
+                print(thirdword,fourthword,firstword,secondword)
+        else: 
+            print(thirdword,secondword,fourthword,firstword)
+
 else:
     if thirdword>fourthword:
         if secondword>fourthword:
             if thirdword>firstword:
                 if secondword>firstword:
-                    print(firstword,secondword,fourthword,thirdword)
+                    if fourthword>firstword:
+                        print(firstword,fourthword,secondword,thirdword)
+                    else:
+                        print(fourthword,firstword,secondword,thirdword)
+
+                else:
+                    print(fourthword,secondword,firstword,thirdword)
+            else:
+                print(fourthword,secondword,thirdword,firstword)
+        else:
+            if thirdword>firstword:
+                if fourthword>firstword:
+                    if secondword>firstword:
+                        print(firstword,secondword,fourthword,thirdword)
+                    else:
+                        print(secondword,firstword,fourthword,thirdword)
+                else:
+                    print(secondword,fourthword,firstword,thirdword)
+            else:
+                print(secondword,fourthword,thirdword,firstword)
     else:
         if fourthword>firstword:
+            if thirdword>firstword:
+                if secondword>firstword:
+                    print(firstword,secondword,thirdword,fourthword)
+                else:
+                    print(secondword,firstword,thirdword,fourthword)
+            else:
+                print(secondword,thirdword,firstword,fourthword)
+        else:
             print(secondword,thirdword,fourthword,firstword)
-#print the results from what was determined
-print()
+
+
