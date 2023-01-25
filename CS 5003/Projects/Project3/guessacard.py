a089f7730913ee853ed120a5aad995a9c6674294
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-01-25 14:18:25 -0500

    corrected the call functions

--- CS 5003/Projects/Project3/guessacard.py
+++ CS 5003/Projects/Project3/guessacard.py
@@ -6,39 +6,27 @@ import random
 def getSuit():
     # pick a random suit for a card. 
     suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-    random_suit = random.choice(suits)
-    return random_suit
+    return random.choice(suits)
 
 def getValue():
     # pick a random value for a card
     value = ['Ace', '2', '3', '4','5', '6', '7', '8', '9' , '10' , 'Jack' , 'Queen', 'King']
-    random_value = random.choice(value)
-    return random_value
-def printCard():
+    return random.choice(value)
+def printCard(value, suits):
     # print out the value of the card (ie Ace of Spades or 10 of Clubs)
     # to the user based on the value and suit that are the parameters.
-    print (getValue() + " of " + getSuit())
+    print(value + " of " + suits)
+    print(getColor(suits))
     pass
-def color():
-    if getSuit() == "Diamonds":
-        color = "red"
-    else:     
-        if getSuit() == "Hearts":
-            color = "red"
-        else:
-            if getSuit() == "Clubs":
-                color = "black"
-            else:
-                if getSuit() == "Spades":
-                    color = "black"
-    print(color)
-
-    
+def getColor(suit):
+    if suit in ["Spades", "Clubs"]:
+        return "Black"
+    else:
+        return "Red"
 def main():
-    getSuit()
-    getValue()
-    printCard()
-    color()
+    suit = getSuit()
+    value = getValue()
+    printCard(value, suit)
 #remember to put in the lines to call main() that are described in part 6
 if __name__ == "__main__":
     main()
