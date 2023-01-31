ea1bf62dd9f890f1769a1da39353385a995d6049
Michael Mills <mmills6060@gmail.com>
2023-01-31 09:53:07 -0500

    working while loop

--- CS 5001/Class Excercises/Lesson 3/whileloop.py
+++ CS 5001/Class Excercises/Lesson 3/whileloop.py
@@ -1,23 +1,18 @@
-# CS5001
+def computeaverage():    
+     #before the loop     
+    num_input=1     
+    total=0     
+    grade=int(input("Enter a grade or -1 to quit "))     
+     #  #loop through all the grades and add to total     
+    while(grade != -1):        
+        total += grade         
+        num_input += 1
+        grade = int(input("Enter a grade or -1 to quit"))     
+              #after the loop-compute average to print   
+    if(num_input>0):        
+        average = total/num_input  
+    else:        
+        average = 0    
+    print("The average grade was: " + str(average))  
 
-def computeAverage():
-        num_inputs = 0
-        total = 0
-        grade = int(input("Enter a grade or -1 to quit "))
-
-        average = total/num_inputs
-        print("The average grade was: " + str(average))
-
-        while(grade != -1):
-            total += grade
-            nun_inputs += 1
-            grade = int(input(("Enter a grade or -1 to quit ")))
-
-        # after the loop, compute average and print
-        if(num_inputs>0):
-            average = total/num_inputs
-        else:
-            average = 0
-        print("The average grade was: " + str(average))
-
-computeAverage()
+computeaverage()
