95ede5caa34ee9e902641b6027d2f7925dafe053
Michael Mills <mmills6060@gmail.com>
2023-01-31 17:31:45 -0500

    orroreven

--- CS 5003/Projects/Project3/oddOrEven.py
+++ CS 5003/Projects/Project3/oddOrEven.py
@@ -1,9 +1,23 @@
+# Michael Arthur Mills
+# Spring 2023, CS 5001
+# Lab 3 code file
+
 import sys
 
-number = ( sys.argv[1] )
 
+# assign the variable number to the second word input in command line terminal
+# check for user input
+
+if len( sys.argv ) > 1:
+    # re-assign the value if the user provided one
+    number = ( sys.argv[1] )
+    my_number = int(number)
+else:
+    number = "10"
+    my_number = int(number)
 # calculate whether a number is odd or even
 def oddOrEven( number ):
+
     print('determining if ' + number + ' is odd or even:')
 
     # if statement
@@ -12,10 +26,9 @@ def oddOrEven( number ):
     else:
         print(number + " is odd.")
 
-# assign values to variables
-my_number = int(number)
-
+def main():
 # call the function
-oddOrEven(number)
+    oddOrEven(number)
 
-
+if __name__ == "__main__":
+    main()
