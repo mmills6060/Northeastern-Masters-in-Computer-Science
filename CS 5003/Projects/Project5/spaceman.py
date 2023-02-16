92541761e8f25112adf8c4f611873d12f19ac9d5
Michael Mills <mmills6060@gmail.com>
2023-02-16 15:10:06 -0500

    completed spaceman wiithout break

--- CS 5003/Projects/Project5/spaceman.py
+++ CS 5003/Projects/Project5/spaceman.py
@@ -1,6 +1,5 @@
 #Project 5
 #Spaceman Implementation
-#Template created by: Lindsay Jamieson
 # Michael Arthur Mills
 # February 15, 2023
 
@@ -57,7 +56,114 @@ def replace_character(rand_word, hidden_word, user_guess,new_hidden_word,guessed
         else:
             new_hidden_word += hidden_word[i]
     return new_hidden_word
+def remove_turn(rand_word, user_guess, guessed):
 
+    '''Checks if a turn should be removed for the user's guess'''
+
+    if user_guess in rand_word or user_guess in guessed:
+        return False
+    else:
+        return True
+    
+def ascii_art(turns_used):
+    if turns_used == 6:
+        return """
+           | |
+           | |
+           |-|
+           |C|
+           |S|
+           |5|
+           |0|
+           |0|
+           |1|
+           | |
+___________|_|_
+        """
+    elif turns_used == 5:
+        return """
+     |     | |
+    / \    | |
+           |-|
+           |C|
+           |S|
+           |5|
+           |0|
+           |0|
+           |1|
+           | |
+___________|_|_ 
+           """
+    elif turns_used == 4:
+        return """
+     |     | |
+    / \    | |
+   |--o|===|-|
+   |---|   |C|
+           |S|
+           |5|
+           |0|
+           |0|
+           |1|
+           | |
+___________|_|_ 
+           """
+    elif turns_used == 3:
+        return """
+     |     | |
+    / \    | |
+   |--o|===|-|
+   |---|   |C|
+  /     \  |S|
+           |5|
+           |0|
+           |0|
+           |1|
+           | |
+___________|_|_ 
+           """
+    elif turns_used == 2:
+        return """
+     |     | |
+    / \    | |
+   |--o|===|-|
+   |---|   |C|
+  /     \  |S|
+ | U     | |5|
+ | S     |=|0|
+           |0|
+           |1|
+           | |
+___________|_|_ 
+           """
+    elif turns_used == 1:
+        return """
+     |     | |
+    / \    | |
+   |--o|===|-|
+   |---|   |C|
+  /     \  |S|
+ | U     | |5|
+ | S     |=|0|
+ | A     | |0|
+ |_______| |1|
+           | |
+___________|_|_ 
+           """
+    elif turns_used == 0:
+        return"""
+     |     | |
+    / \    | |
+   |--o|===|-|
+   |---|   |C|
+  /     \  |S|
+ | U     | |5|
+ | S     |=|0|
+ | A     | |0|
+ |_______| |1|
+  |@| |@|  | |
+___________|_|_   
+          """
 def main():
     '''Main game playing location'''
     #define number of turns allowed as 6. Create empty variable for new hidden word. 
@@ -66,42 +172,37 @@ def main():
     
     #create welcome message
     print("Welcome to The Spaceman Game, created by Michael Arthur Mills ")
-    print("""
-     |     | |
-    / \    | |
-   |--o|===|-|
-   |---|   |d|
-  /     \  |w|
- | U     | |b|
- | S     |=| |
- | A     | | |
- |_______| |_|
-  |@| |@|  | |
-___________|_|_   
-          """)
+
     #call funcions that pick a random word, then convert it to *
     rand_word = pick_random_word()
     hidden_word = (create_hidden_word(rand_word))
 
     guessed = set() # create emptty set for guessed
 
-    # while the user has not guessed the word correctly, ask the user to input a guess,
+    # while the user has not guessed tse word correctly, ask the user to input a guess,
     # subtract a turn, call function that replaces correctly guessed charaters,
     # call function to see if its correct, print stuff so user knows whats going on.
-    while True:
-        if turns_used >= 1:
-            user_guess = (input("Enter your guess: "))
+    game_over = False
+
+    while not game_over and turns_used >= 1:
+        user_guess = input("Enter your guess: ")
+        new_hidden_word = replace_character(rand_word, hidden_word, user_guess, new_hidden_word, guessed)
+        is_turn_removed = remove_turn(rand_word, user_guess, guessed)
+        if is_turn_removed:
             turns_used -= 1
-            new_hidden_word = replace_character(rand_word, hidden_word, user_guess,new_hidden_word,guessed)
-            if word_found(rand_word, new_hidden_word):
-                print("Congratulations, you won the game!")
-                break
+        if word_found(rand_word, new_hidden_word):
+            print("Congratulations, you won the game!")
+            game_over = True
+        else:
             print(user_guess)
             print("You have " + str(turns_used) + " turns left.")
+            print(ascii_art(turns_used))
             print(new_hidden_word)
-        else:
-            print("Sorry, better luck next time")
-            break
+
+    if not game_over:
+        print(ascii_art(turns_used))
+        print("Sorry, better luck next time")
+
 
 if __name__ == "__main__":
     main() 
