f8f2d8cd0bc1e91d0f1da1f20b6ce1925621e17f
Michael Mills <mmills6060@gmail.com>
2023-02-16 11:35:01 -0500

    working spaceman. just need to add turns used

--- CS 5003/Projects/Project5/spaceman.py
+++ CS 5003/Projects/Project5/spaceman.py
@@ -22,46 +22,49 @@ def create_hidden_word(rand_word):
     '''
     hidden_word = "*" * len(rand_word)
     print(hidden_word)
-def word_found(rand_word, hidden_word):
+    return hidden_word
+def word_found(rand_word, new_hidden_word):
     '''word_found will detect whether the random word has been uncovered
     Input: the random word and the hidden version of the word
     Output: True if the hidden word has all been revealed, False otherwise
     '''
-    if rand_word == hidden_word:
+    if rand_word == new_hidden_word:
+        return True
+    else:
         return False
 
 
-def replace_character(rand_word, hidden_word, user_guess):
+def replace_character(rand_word, hidden_word, user_guess,new_hidden_word,guessed):
     '''Replaces all the occurences of user_guess in hidden_word with user_guess
     Input: the random word, the hidden word so far, and the user guess
     Output: the new hidden word
     '''
+    new_hidden_word = ''
+    for i in range(len(rand_word)):
+        if rand_word[i] == user_guess or rand_word[i] in guessed:
+            new_hidden_word += rand_word[i]
+            guessed.add(rand_word[i])
+        else:
+            new_hidden_word += hidden_word[i]
+    return new_hidden_word
 
-    # Find all occurrences of user_guess in hidden_word
-    guess_indices = [i for i in range(
-        len(rand_word)) if rand_word[i] == user_guess]
-
-    # Replace each occurrence of user_guess with user_guess in hidden_word
-    for i in guess_indices:
-        hidden_word = hidden_word[:i] + user_guess + hidden_word[i+1:]
-
-    return hidden_word
-
-
-    print(hidden_word)
 def main():
     '''Main game playing location'''
     turns_used = 0
+    new_hidden_word = ""
+    
     print("Welcome to The Spaceman Game, created by Michael Arthur Mills")
     rand_word = pick_random_word()
     hidden_word = (create_hidden_word(rand_word))
+    guessed = set() # create emptty set for guessed
     while True:
         user_guess = (input("Enter your guess: "))
         turns_used += 1
-        replace_character(rand_word, hidden_word, user_guess)
+        new_hidden_word = replace_character(rand_word, hidden_word, user_guess,new_hidden_word,guessed)
+        if word_found(rand_word, new_hidden_word):
+            print("Congratulations, you won the game!")
+            break
         print(user_guess)
-        print(hidden_word)
-    else:
-        print("Congratulations, you won the game!")
+        print(new_hidden_word)
 if __name__ == "__main__":
-    main()
+    main() 
