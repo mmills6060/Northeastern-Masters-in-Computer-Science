daebc01f8374e9360bb4ce2890accef2076951ae
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2023-03-23 12:57:57 -0400

    Update README.py

---
+++ CS 5003/Projects/Project8/README.py
@@ -0,0 +1,12 @@
+Michael Arthur Mills
+March 23, 2023
+CS5001
+Project 8: Ice Cream Shoppe
+Problem Description
+This project consists of writing python code to replicate the way in which an ice cream shop works. More specifically, the python code calculates the amount of ice cream, and cartons of ice cream used based on the volume of the scoop as well as the number of scoops used. This project uses multiple different concepts in Python. Some notable concepts include calling other python files, classes, and methods. This was a project to help students grow comfortable working with classes and how they operate.
+Required Task Elements
+In retrospect, I thought that this was one of the easier projects to work on. I wasn’t necessarily stuck on any bugs for several hours or stuck on how to implement a particular block of code. The template files that contained comments about what should be implemented really helped me understand what needed to be done. Psychologically, I rarely needed to think about the entire program as a whole, and was able to focus on the smaller segments of the program one at a time, which made the overall project much less overwhelming. With that in mind, I was able to think about a smaller problem such as writing code to simply ask the user how many scoops of ice cream they would like to have, instead of a broader problem like replicating an entire ice cream shop.
+Reflection
+This was a particularly interesting project for me. This was the first time where I felt like I was looking at a mathematical equation and trying to figure out how I can translate a mathematical equation to python code…or any code. I thought it was interesting how I really had to understand what a class is and how it behaves in order for me to properly complete this project. I also am growing more and more comfortable with unit tests, and feel like they are becoming more and more useful to test segments of code that would be otherwise impossible to test. I also had to have knowledge on the syntax, how to import things from another file, and the general syntax behind classes.
+Acknowledgements
+o I did not use any resources to help me in this project, besides the template files provided.
