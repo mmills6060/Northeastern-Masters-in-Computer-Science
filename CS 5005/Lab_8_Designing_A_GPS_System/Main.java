36054dfb7eb22b6fcfd110021179fcb6f452546f
Michael <mmills6060@gmail.com>
2023-07-28 11:22:51 -0400

    java final

--- CS 5005/Lab_8_Designing_A_GPS_System/Main.java
+++ CS 5005/Lab_8_Designing_A_GPS_System/Main.java
@@ -1,27 +1,33 @@
+// Project created by Michael Arthur Mills
+
 // Main.java (Driver File)
 public class Main {
     public static void main(String[] args) {
+        // Create instances of Model, View, and Controller
         Model model = new Model();
         View view = new View();
         Controller controller = new Controller(model, view);
 
-        // Example usage
+
+        // Calculate distance between two points using latitude and longitude
         double lat1 = 43.8523;
         double lon1 = 69.6281;
         double lat2 = 43.5637;
         double lon2 = 70.2000;
-
         controller.calculateDistance(lat1, lon1, lat2, lon2);
 
+        // Calculate time to travel a distance at a specific speed
         double distance = 56; // miles
         double speed = 60; // mph
         controller.calculateTime(distance, speed);
 
+        // Calculate time remaining until refueling
         double fuelEfficiency = 30; // miles per gallon
         double currentSpeed = 50; // mph
         double remainingFuel = 10; // gallons
         controller.calculateTimeToRefuel(fuelEfficiency, currentSpeed, remainingFuel);
 
+        // Calculate time needed to bring a car to a complete stop
         double currentSpeed2 = 40; // mph
         double distanceToStop = 50; // feet
         double decelerationRate = 10; // ft/s^2
--- CS 5005/Lab_8_Designing_A_GPS_System/Model.java
+++ CS 5005/Lab_8_Designing_A_GPS_System/Model.java
@@ -1,35 +1,43 @@
 // Model.java
 public class Model {
+
+    // Method to calculate distance between two points using their latitude and longitude
     public double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        // Implementation to calculate distance between two points using their latitude and longitude
-        double earthRadius = 6371; // Earth's radius in kilometers
+        // Earth's radius in kilometers
+        double earthRadius = 6371;
+        
+        // Convert latitude and longitude to radians
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
 
+        // Haversine formula to calculate distance
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
                 * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        double distance = earthRadius * c;
+        double distance = earthRadius * c; // Distance in kilometers
         return distance;
     }
 
+    // Method to calculate time to travel a distance at a specific speed
     public double calculateTime(double distance, double speed) {
-        // Implementation to calculate time to travel a distance at a specific speed
+        // Time = Distance / Speed
         double time = distance / speed;
         return time;
     }
 
+    // Method to calculate time remaining until refueling
     public double calculateTimeToRefuel(double fuelEfficiency, double currentSpeed, double remainingFuel) {
-        // Implementation to calculate time remaining until refueling
+        // Time to refuel = Remaining fuel / (Fuel efficiency * (1 or -1 based on whether the car is moving or not))
         double timeToRefuel = remainingFuel / (fuelEfficiency * (currentSpeed > 0 ? 1 : -1));
         return timeToRefuel;
     }
 
+    // Method to calculate time needed to bring a car to a complete stop
     public double calculateStoppingTime(double currentSpeed, double distanceToStop, double decelerationRate) {
-        // Implementation to calculate time needed to bring a car to a complete stop
+        // Time to stop = Current speed / Deceleration rate
         double stoppingTime = currentSpeed / decelerationRate;
         return stoppingTime;
     }
