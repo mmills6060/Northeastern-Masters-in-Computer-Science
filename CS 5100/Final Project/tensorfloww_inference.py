f6d05e57890bcf47d163f4e6488a6d9fc76cfa22
mmills6060 <mmills6060@gmail.com>
2023-12-07 17:08:17 -0500

    working inference and training

---
+++ CS 5100/Final Project/tensorfloww_inference.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import numpy as np
+import os
+import time
+import keras.utils
+
+
+
+
+one_step_reloaded = tf.saved_model.load('one_step')
+
+states = None
+next_char = tf.constant(['ROMEO:'])
+result = [next_char]
+
+for n in range(100):
+  next_char, states = one_step_reloaded.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+print(tf.strings.join(result)[0].numpy().decode("utf-8"))
+
